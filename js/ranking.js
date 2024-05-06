ce3e3d3e20c657c6d9260b10e03719bb1591c2eb
ArnauSanchezAbad <u1986965@campus.udg.edu>
2024-05-06 17:38:06 +0200

    Arnau Branch v1.01
    Afegit css al menú i un sistema de rankings.

--- js/ranking.js
+++ js/ranking.js
@@ -1,5 +1,28 @@
 addEventListener('load', function() {
-    document.getElementById('return').addEventListener('click', function(){
-        window.location.assign("../");
+    const divPuntuacions = document.getElementById("puntuacions");   
+    const ranking = localStorage.getItem("ranking");
+    console.log("Ranking:", ranking);
+
+    const ul = document.createElement("ul");
+
+    if (ranking === null || ranking === "[]") {
+        console.log("No hi ha ranking");
+        const text = document.createElement("text");
+        text.textContent = "No hi ha puntuacions disponibles.";
+        ul.appendChild(text);
+    } 
+    else {
+        const listaRanking = JSON.parse(ranking);
+        listaRanking.forEach((entry, index) => {
+            const li = document.createElement("li");
+            li.textContent = `${index + 1}: ${entry.nom} | ${entry.puntuacio} punts`;
+            ul.appendChild(li);
+        });
+    }
+    divPuntuacions.appendChild(ul);
+
+    document.getElementById('return').addEventListener('click', 
+    function(){
+    window.location.assign("../");
     });
 });
